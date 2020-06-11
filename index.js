8040af1745b53e15b28766d5d830325e7441bfb4
Pankaj Soam <pankaj.soam111@gmail.com>
2020-06-12 02:39:12 +0530

    set up one controller and one route and set ejs as view engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 
 app.use('/', require('./routes'));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
 app.listen(port, function(err){
     if(err){
         console.log('error:', err); //instead of this line we can use console.log(`error in running the server : ${err}`);
